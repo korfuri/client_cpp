c2309f0355a17e3a2f7065512a856d7bda2c0e17
Uriel Corfa <uriel@corfa.fr>
2015-07-25 19:33:37 -0700

    Test that counters and gauges work properly in a multithreaded environment.

--- prometheus/client_concurrent_test.cc
+++ prometheus/client_concurrent_test.cc
@@ -1,18 +1,67 @@
 #include "client.hh"
 #include <gtest/gtest.h>
 #include <list>
+#include <string>
 #include <thread>
 
 namespace {
 
+using namespace prometheus;
+
 class ClientConcurrentTest : public ::testing::Test {};
 
-  TEST_F(ClientConcurrentTest, CounterTest) {
-    std::list<std::thread> l;
-    for (int i = 0; i < 40; ++i) {
-      
-    }
+Counter<0> c0("test_counter0");
+Counter<1> c1("test_counter1", {{"even"}});
+
+const int kIterations = 1000;
+const int kThreads = 40;
+
+void f_countertest(int threadid) {
+  for (int i = 0; i < kIterations; ++i) {
+    c0.inc();
+    c1.labels({{std::to_string(bool(threadid % 2))}}).inc();
   }
+}
+
+TEST_F(ClientConcurrentTest, CounterTest) {
+  std::list<std::thread> l;
+  for (int i = 0; i < kThreads; ++i) {
+    l.push_back(std::thread(f_countertest, i));
+  }
+  for (auto& t : l) {
+    t.join();
+  }
+  EXPECT_EQ(kThreads * kIterations, c0.value());
+  EXPECT_EQ(kThreads * kIterations / 2, c1.labels({{"0"}}).value());
+  EXPECT_EQ(kThreads * kIterations / 2, c1.labels({{"1"}}).value());
+}
+
+Gauge<0> g0("test_gauge0");
+Gauge<1> g1("test_gauge1", {{"even"}});
+
+void f_gaugetest(int threadid) {
+  g0.set(threadid);
+  g1.labels({{std::to_string(bool(threadid % 2))}}).set(threadid);
+}
+
+TEST_F(ClientConcurrentTest, GaugeTest) {
+  std::list<std::thread> l;
+  for (int i = 0; i < kThreads; ++i) {
+    l.push_back(std::thread(f_gaugetest, i));
+  }
+  for (auto& t : l) {
+    t.join();
+  }
+  EXPECT_TRUE(g0.value() >= 0 && g0.value() < kThreads);
+  double g1_even = g1.labels({{"0"}}).value();
+  EXPECT_TRUE(g1_even >= 0);
+  EXPECT_TRUE(g1_even < kThreads);
+  EXPECT_EQ(0, (int)g1_even % 2);
+  double g1_odd = g1.labels({{"1"}}).value();
+  EXPECT_TRUE(g1_odd >= 0);
+  EXPECT_TRUE(g1_odd < kThreads);
+  EXPECT_EQ(1, (int)g1_odd % 2);
+}
 
 }
 
